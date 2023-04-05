9a11473a48788ef17ad9a1bbbbb4ac44b7ba1de3
Danzhin <dronzhin123@gmail.com>
2023-04-05 15:21:17 +0300

    v0.004

--- ObjectHelper.c
+++
@@ -1,7 +0,0 @@
-#include "templates.h"  
-#include "Object.h"
-#include "ExampleClass.h"
- 
-#undef T  
-#define T ExampleClass  
-#include "Object.c"    
--- ObjectHelper.h
+++
@@ -1,7 +0,0 @@
-#pragma once  
-#include "templates.h" 
-#include "ExampleClass.h"
- 
-#undef T 
-#define T ExampleClass  
-#include "Object.h"  
