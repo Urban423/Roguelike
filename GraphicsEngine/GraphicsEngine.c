ed0f7c131eea56022f44b22b26bc85d964ab3655
Urban423 <urbanzak21@gmail.com>
2023-04-17 00:00:15 +0300

    v0.14.1

--- GraphicsEngine/GraphicsEngine.c
+++ GraphicsEngine/GraphicsEngine.c
@@ -474,9 +474,9 @@ void BufferDrawObject(Buffer* buffer, Matrix3x3 world_pos, VertexMesh* mesh, Tex
 	for(unsigned int i = 0; i < mesh->faces_size; i += 3)	
 	{
 		DrawTriangleByTexture(buffer, 
-				copy_of_verticles[mesh->faces[i]].x,  copy_of_verticles[mesh->faces[i]].y,
-				copy_of_verticles[mesh->faces[i + 1]].x, copy_of_verticles[mesh->faces[i + 1]].y,
-				copy_of_verticles[mesh->faces[i + 2]].x, copy_of_verticles[mesh->faces[i + 2]].y,
+				copy_of_verticles[mesh->faces[i]].x - 15,  copy_of_verticles[mesh->faces[i]].y- 15,
+				copy_of_verticles[mesh->faces[i + 1]].x - 15, copy_of_verticles[mesh->faces[i + 1]].y- 15,
+				copy_of_verticles[mesh->faces[i + 2]].x- 15, copy_of_verticles[mesh->faces[i + 2]].y- 15,
 				texture, 
 				mesh->UV_map[mesh->faces[i]],
 				mesh->UV_map[mesh->faces[i + 1]],
